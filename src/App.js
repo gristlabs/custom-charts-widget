7ba0d44d503b6d70716c78887efef957728c5c08
Alex Hall <alex.mojaki@gmail.com>
2023-09-29 14:36:42 +0200

    use col refs to handle column renames

--- src/App.js
+++ src/App.js
@@ -42,17 +42,38 @@ function produceFilledInData(obj, dataSources) {
 
 const { grist } = window;
 
+async function getColumns() {
+  const tableId = await grist.selectedTable.getTableId();
+  const tables = await grist.docApi.fetchTable('_grist_Tables');
+  const columns = await grist.docApi.fetchTable('_grist_Tables_column');
+  const tableRef = tables.id[tables.tableId.indexOf(tableId)];
+  const columnRecords = columns.id.map((_id, i) =>
+    Object.fromEntries(Object.keys(columns).map(key => [key, columns[key][i]]))
+  );
+  return columnRecords.filter(col => col.parentId === tableRef);
+}
+
 class App extends Component {
   constructor(props) {
     super(props);
     setAutoFreeze(false);
-    this.state = { data: [], layout: {}, frames: [], dataSources: {} };
+    this.state = { data: [], layout: {}, frames: [], dataSources: {}, dataSourceOptions: [] };
 
     const onGristUpdate = async () => {
-      const dataSources = await grist.fetchSelectedTable();
+      const columns = await getColumns();
+      const colIdToRef = Object.fromEntries(columns.map(col => [col.colId, col.id]));
+      const tableData = await grist.fetchSelectedTable();
+      const dataSources = Object.fromEntries(Object.entries(tableData).map(
+        ([colId, values]) => [colIdToRef[colId], values]
+      ));
+      const dataSourceOptions = columns.map(col => ({
+        value: col.id,
+        label: col.label
+      })).filter(col => col.value in dataSources);
+
       const state = await grist.getOption('state');
       const data = produceFilledInData(state?.data, dataSources);
-      this.setState({ ...state, data, dataSources });
+      this.setState({ ...state, data, dataSources, dataSourceOptions });
     };
     grist.onRecords(onGristUpdate);
     grist.onOptions(onGristUpdate);
@@ -60,9 +81,6 @@ class App extends Component {
   }
 
   render() {
-    const dataSourceOptions = Object.keys(this.state.dataSources).map((name) => ({
-      value: name, label: name,
-    }));
     return (<div className="app">
       <PlotlyEditor
         data={this.state.data}
@@ -70,11 +88,11 @@ class App extends Component {
         config={config}
         frames={this.state.frames}
         dataSources={this.state.dataSources}
-        dataSourceOptions={dataSourceOptions}
+        dataSourceOptions={this.state.dataSourceOptions}
         plotly={plotly}
         onUpdate={(data, layout, frames) => {
           this.setState({ data, layout, frames });
-          const emptyDataSources = Object.fromEntries(dataSourceOptions.map(({value}) => [value, []]));
+          const emptyDataSources = Object.fromEntries(this.state.dataSourceOptions.map(({ value }) => [value, []]));
           data = produceFilledInData(data, emptyDataSources);
           grist.setOption('state', { data, layout, frames });
         }}
